659733ae244f1954d388f73faf76e694cff4a93b
03hoho03 <ghwjd321@naver.com>
2023-09-27 11:58:59 +0900

    create: 메인 페이지

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,18 @@
+// import cn from 'classnames'
+import Carousel from './components/Carousel'
+import style from './page.module.css'
+
 export default function Home() {
+  const monthCatsImages = ['./logo.svg', './logo2.svg', './cat.jpg']
   return (
     <div>
-      hello world
+      <div className={style.month_cats}>
+        <h3>이달의 고양이들</h3>
+        <Carousel carouselList={monthCatsImages} autoPlay />
+        <div className={style.month_cats_img_div}>
+          <img src="./cat.jpg" alt="best-cat" />
+        </div>
+      </div>
     </div>
   )
 }
