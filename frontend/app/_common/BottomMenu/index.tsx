f8ac94e98626032cbba37c520ef034a0200f58f1
03hoho03 <ghwjd321@naver.com>
2023-10-07 14:58:54 +0900

    Feat 하단메뉴 바

---
+++ frontend/app/_common/BottomMenu/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHouse,
+  faClipboard,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons'
+import { faBell, faUser } from '@fortawesome/free-regular-svg-icons'
+import Link from 'next/link'
+import style from './bottomMenu.module.css'
+
+const userTools = () => (
+  <div className={style.main}>
+    <Link href="/" className={style.link}>
+      <button type="button" className={style.button}>
+        <FontAwesomeIcon icon={faHouse} className={style.icon} />
+        <span>메인</span>
+      </button>
+    </Link>
+    <Link href="/feed" className={style.link}>
+      <button type="button" className={style.button}>
+        <FontAwesomeIcon icon={faClipboard} className={style.icon} />
+        <span>피드</span>
+      </button>
+    </Link>
+    <Link href="search" className={style.link}>
+      <button type="button" className={style.button}>
+        <FontAwesomeIcon icon={faMagnifyingGlass} className={style.icon} />
+        <span>검색</span>
+      </button>
+    </Link>
+    <Link href="alarm" className={style.link}>
+      <button type="button" className={style.button}>
+        <FontAwesomeIcon icon={faBell} className={style.icon} />
+        <span>알림</span>
+      </button>
+    </Link>
+    <Link href="profile" className={style.link}>
+      <button type="button" className={style.button}>
+        <FontAwesomeIcon icon={faUser} className={style.icon} />
+        <span>내 정보</span>
+      </button>
+    </Link>
+  </div>
+)
+
+export default userTools
