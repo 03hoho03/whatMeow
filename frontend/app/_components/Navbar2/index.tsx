f79354f370e10203c645634d3c352877caaafac8
03hoho03 <ghwjd321@naver.com>
2023-10-18 15:20:31 +0900

    Init 상단네비게이션
    로그인 상태에 따른 버튼 노출

--- frontend/app/_components/Navbar2/index.tsx
+++ frontend/app/_components/Navbar2/index.tsx
@@ -1,13 +1,18 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
+
 import cn from 'classnames'
 import Link from 'next/link'
 import style from './navbar2.module.css'
+import {useRouter} from 'next/navigation'
+import { useRecoilState } from 'recoil'
+import { authState } from '@/app/_store/atom/auth'
 
 function Navbar2() {
   const [scrollY, setScrollY] = useState(0)
-
+  const [auth,setAuth] = useRecoilState(authState);
+  const router = useRouter();
   useEffect(() => {
     const handleScroll = () => {
       // window.scrollY를 사용하여 현재 스크롤 위치를 가져옵니다.
@@ -22,13 +27,26 @@ function Navbar2() {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+  const onHandleLogout = async() => {
+    try {
+      await fetch('api/account/logout')
+      setAuth(false)
+      router.push('/')
+    }
+    catch(error) {
+      console.error(error)
+    }
+  }
   return (
     <div className={cn(style.main, { [style.scrolled]: scrollY > 0 })}>
       <div>왓냐옹</div>
       <div>
+        {auth ? 
+        <button className={style.navBtn} onClick={onHandleLogout}>로그아웃</button> :
         <Link href="/login" className={style.navBtn}>
           로그인
         </Link>
+        }
       </div>
     </div>
   )
