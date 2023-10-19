576e58e47b4ce89e3179408007bb824d40f85ff7
03hoho03 <ghwjd321@naver.com>
2023-10-19 20:22:06 +0900

    Init 검색 컴포넌트

---
+++ frontend/app/(route)/search/_components/SearchInput/index.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import style from './searchInput.module.css'
+
+const SearchInput = () => {
+  return (
+    <div className={style.main_wrapper}>
+      <form className={style.search_form}>
+        <input className={style.search_input} placeholder='검색어를 입력해주세요'/>
+      </form>
+    </div>
+  )
+}
+
+export default SearchInput
