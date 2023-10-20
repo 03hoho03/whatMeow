5410ad04649512325181389ca45ad560a1c2b2e2
03hoho03 <ghwjd321@naver.com>
2023-10-21 01:08:34 +0900

    Style eslint 적용

--- frontend/app/_components/Navbar2/index.tsx
+++ frontend/app/_components/Navbar2/index.tsx
@@ -4,26 +4,26 @@ import React, { useEffect, useState } from 'react'
 
 import cn from 'classnames'
 import Link from 'next/link'
-import style from './navbar2.module.css'
-import {useRouter} from 'next/navigation'
+import { useRouter, usePathname } from 'next/navigation'
 import { useRecoilState } from 'recoil'
 import { authState } from '@/app/_store/atom/auth'
-import { usePathname } from 'next/navigation'
+
+import style from './navbar2.module.css'
 
 const navItems = [
-  {name:'투데이' ,path:'/'},
-  {name:'피드', path:'/feed'},
-  {name:'검색', path:'/search'},
-  {name:'알림',path:'/alarm'},
-  {name:'프로필',path:'/profile'}
+  { name: '투데이', path: '/' },
+  { name: '피드', path: '/feed' },
+  { name: '검색', path: '/search' },
+  { name: '알림', path: '/alarm' },
+  { name: '프로필', path: '/profile' },
 ]
 
 function Navbar2() {
   const [scrollY, setScrollY] = useState(0)
-  const [auth,setAuth] = useRecoilState(authState);
-  
-  const pathname = usePathname();
-  const router = useRouter();
+  const [auth, setAuth] = useRecoilState(authState)
+
+  const pathname = usePathname()
+  const router = useRouter()
   useEffect(() => {
     const handleScroll = () => {
       setScrollY(window.scrollY)
@@ -34,43 +34,65 @@ function Navbar2() {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
-  const onHandleLogout = async() => {
+  const onHandleLogout = async () => {
     try {
       await fetch('api/account/logout')
       setAuth(false)
       router.push('/')
-    }
-    catch(error) {
+    } catch (error) {
       console.error(error)
     }
   }
   return (
-    <section className={cn(style.main_wrapper, { [style.scrolled]: scrollY > 0 })}>
+    <section
+      className={cn(style.main_wrapper, { [style.scrolled]: scrollY > 0 })}
+    >
       <h3 className={style.logo_wrapper}>
-        <Link href='/'>LOGO</Link>
+        <Link href="/">LOGO</Link>
       </h3>
       <nav className={style.nav}>
         <ul className={style.nav_ul}>
-          {navItems.map(({name,path},idx)=>
+          {navItems.map(({ name, path }, idx) => (
             <li key={`${name}-${idx}`}>
-              <Link href={path} className={cn(style.nav_link,{[style.nav_clicked]: pathname===path })}>{name}</Link>
+              <Link
+                href={path}
+                className={cn(style.nav_link, {
+                  [style.nav_clicked]: pathname === path,
+                })}
+              >
+                {name}
+              </Link>
             </li>
-          )}
+          ))}
         </ul>
       </nav>
       <ul className={style.util_ul}>
-        {auth ?
-        <li>
-          <button className={cn(style.util_button,{[style.util_scrolled]: scrollY > 0})} onClick={onHandleLogout}>로그아웃</button>
-        </li> :
-        <li>
-          <Link href="/login">
-            <button  className={cn(style.util_button,{[style.util_scrolled]: scrollY > 0})}>
-              로그인
+        {auth ? (
+          <li>
+            <button
+              className={cn(style.util_button, {
+                [style.util_scrolled]: scrollY > 0,
+              })}
+              onClick={onHandleLogout}
+              type="button"
+            >
+              로그아웃
             </button>
-          </Link>
-        </li>
-        }
+          </li>
+        ) : (
+          <li>
+            <Link href="/login">
+              <button
+                className={cn(style.util_button, {
+                  [style.util_scrolled]: scrollY > 0,
+                })}
+                type="button"
+              >
+                로그인
+              </button>
+            </Link>
+          </li>
+        )}
       </ul>
     </section>
   )
