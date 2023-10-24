3343133b36308b5030e28590cca42df58a6a20f6
03hoho03 <ghwjd321@naver.com>
2023-10-24 17:46:21 +0900

    Refactor submit버튼 컴포넌트화 및 prettier 적용

--- frontend/app/(route)/(auth)/login/_components/LoginForm/index.tsx
+++ frontend/app/(route)/(auth)/login/_components/LoginForm/index.tsx
@@ -1,52 +1,56 @@
 'use client'
 
 import React from 'react'
-import cn from 'classnames'
 import { useRouter } from 'next/navigation'
 import style from './loginForm.module.css'
-import { SubmitHandler, useForm} from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { useUserService } from '@/app/_services/userService'
 import { useSetRecoilState } from 'recoil'
 import { authState } from '@/app/_store/atom/auth'
+import SubmitBtn from '@/app/_common/SubmitBtn'
 
 interface LoginFormValue {
-  email:string,
-  password:string,
+  email: string
+  password: string
 }
 
 function LoginForm() {
-  const {register,handleSubmit,formState:{isValid,errors}} = useForm<LoginFormValue>();
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<LoginFormValue>()
   const setAuth = useSetRecoilState(authState)
-  
+
   const router = useRouter()
   const userService = useUserService()
 
   const fields = {
-    email:register('email',{
-      required:'이메일을 입력해주세요.',
+    email: register('email', {
+      required: '이메일을 입력해주세요.',
       pattern: /^\S+@\S+$/i,
-      maxLength:{
-        value:24,
-        message:'이메일은 최대 24자까지 가능합니다.'
-      }
+      maxLength: {
+        value: 24,
+        message: '이메일은 최대 24자까지 가능합니다.',
+      },
     }),
-    password:register('password',{
-      required:'비밀번호를 입력해주세요.',
+    password: register('password', {
+      required: '비밀번호를 입력해주세요.',
       minLength: {
-        value:6,
-        message:'비밀번호는 최소 6자 이상 설정해주세요.'
+        value: 6,
+        message: '비밀번호는 최소 6자 이상 설정해주세요.',
       },
       maxLength: {
-        value:24,
-        message:'비밀번호는 최대 24자 이하로 설정해주세요.'
-      }
-    })
+        value: 24,
+        message: '비밀번호는 최대 24자 이하로 설정해주세요.',
+      },
+    }),
   }
-  const onHandleLogin:SubmitHandler<LoginFormValue> = async (data:any) => {
-    const {email, password} = data;
-    const response = await userService.login(email,password);
-    setAuth(true);
-    router.push('/');
+  const onHandleLogin: SubmitHandler<LoginFormValue> = async (data: any) => {
+    const { email, password } = data
+    await userService.login(email, password)
+    setAuth(true)
+    router.push('/')
   }
   return (
     <form className={style.main_wrapper} onSubmit={handleSubmit(onHandleLogin)}>
@@ -73,17 +77,9 @@ function LoginForm() {
           </div>
         </div>
       </div>
-      <div className={style.login_btn_container}>
-        <button
-          type="submit"
-          className={cn(style.login_btn, {
-            [style.is_login_enabled]: isValid,
-          })}
-          disabled={!isValid}
-        >
-          <span>로그인</span>
-        </button>
-      </div>
+      <SubmitBtn isValid={isValid}>
+        <span>로그인</span>
+      </SubmitBtn>
     </form>
   )
 }
