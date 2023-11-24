035d0fc25b7de56ec3e5c035fea7b620dccc3433
fnzksxl <fnzksxl@gmail.com>
2023-11-24 18:21:15 +0900

    #31 AWS Secret Key 교체

--- backend/app/config.py
+++ backend/app/config.py
@@ -31,8 +31,8 @@ class Settings:
     s3 = boto3.client(
         "s3",
         region_name="ap-northeast-2",
-        aws_access_key_id="AKIA3WNTAIC452OB5AWS",
-        aws_secret_access_key="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",
+        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
         config=boto3.session.Config(signature_version="s3v4"),
     )
 
