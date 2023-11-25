ff6e491bfaaf57ff0d3b3e47e903a43e191c9c4b
fnzksxl <fnzksxl@gmail.com>
2023-11-26 00:09:15 +0900

    Like 유무 여부 조회 엔드포인트 작성

--- backend/app/api/v1/like/like.py
+++ backend/app/api/v1/like/like.py
@@ -8,7 +8,8 @@ from app.database import get_db
 router = APIRouter(tags=["Like"])
 
 
-@router.get("/{post_id}", status_code=status.HTTP_202_ACCEPTED)
+# 좋아요 누르는 엔드포인트
+@router.post("/{post_id}", status_code=status.HTTP_202_ACCEPTED)
 async def like(
     request: Request,
     post_id: int,
@@ -24,3 +25,23 @@ async def like(
             return {"status": stat}
         else:
             raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Failed to like")
+
+
+@router.get("/{post_id}", status_code=status.HTTP_200_OK)
+async def is_like(
+    request: Request,
+    post_id: int,
+    db: Session = Depends(get_db),
+):
+    access_token = request.cookies.get("accessToken")
+    decoded_dict = await auth_utils.verify_access_token(access_token)
+    if decoded_dict:
+        stat = await like_utils.is_like(post_id, decoded_dict.get("id"), db)
+        if stat == "YES":
+            return {"status": stat}
+        elif stat == "NO":
+            return {"status": stat}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detali="Failed to check like or unlike"
+            )
--- backend/app/api/v1/like/like_utils.py
+++ backend/app/api/v1/like/like_utils.py
@@ -9,13 +9,27 @@ async def add_like(post_id, user_id, db):
                 like_row = db.query(model.Like).filter_by(owner_id=user_id).first()
                 db.delete(like_row)
                 db.commit()
-                return "unlike"
+                return "UNLIKE"
 
         like_row = model.Like(owner_id=user_id, post_id=post_id)
         db.add(like_row)
         db.commit()
 
-        return "like"
+        return "LIKE"
     except Exception as e:
         print(f"An error occured : {e}")
-        return "fail"
+        return "FAIL"
+
+
+async def is_like(post_id, user_id, db):
+    try:
+        post_row = db.query(model.Post).filter_by(id=post_id).first()
+        status = "NO"
+        for like in post_row.likes:
+            if like.owner_id == user_id:
+                status = "YES"
+
+        return status
+    except Exception as e:
+        print(e)
+        return "FAIL"
