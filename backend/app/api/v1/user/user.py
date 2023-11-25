ef2e6e123e099790ef8df125c856760a4bc90fc1
fnzksxl <fnzksxl@gmail.com>
2023-11-25 18:59:14 +0900

    #35 User 정보 업데이트 엔드포인트 작성

--- backend/app/api/v1/user/user.py
+++ backend/app/api/v1/user/user.py
@@ -1,9 +1,10 @@
-from fastapi import Depends, APIRouter, status
+from fastapi import Depends, APIRouter, status, UploadFile, File, Form, Request
+from typing import Optional
 from sqlalchemy.orm.session import Session
 
 from app.database import get_db
 
-# from app.api.v1.auth import auth_utils
+from app.api.v1.auth import auth_utils
 from app.api.v1.user import user_utils
 
 router = APIRouter(tags=["User"])
@@ -15,3 +16,20 @@ async def is_duplicated(nickname: str, db: Session = Depends(get_db)):
         return True
     else:
         return False
+
+
+@router.put("/update", status_code=status.HTTP_202_ACCEPTED)
+async def update_userdata(
+    request: Request,
+    nickname: Optional[str] = Form(None),
+    name: Optional[str] = Form(None),
+    explain: Optional[str] = Form(None),
+    file: Optional[UploadFile] = File(None),
+    db: Session = Depends(get_db),
+):
+    access_token = request.cookies.get("accessToken")
+    decoded_dict = await auth_utils.verify_access_token(access_token)
+    if decoded_dict:
+        _dict = {"nickname": nickname, "name": name, "explain": explain}
+        if await user_utils.update_user_info(file, _dict, decoded_dict.get("id"), db):
+            return {"success": True}
