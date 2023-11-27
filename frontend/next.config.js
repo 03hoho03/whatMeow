5f5ea4d1b3eac4333a17a6186f8985af3698f4ad
03hoho03 <ghwjd321@naver.com>
2023-11-28 00:19:55 +0900

    chore 로컬 도메인 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    webpack: (config, { dev }) => {
+      if (dev) {
+        config.devServer = {
+          port: 3001,
+          allowedHosts: ['local.whatmeow.shop'],
+        }
+        config.watchOptions = {
+          poll: 1000,
+          aggregateTimeout: 300,
+        }
+      }
 
-module.exports = nextConfig
+      return config
+    },
+  }
+  return nextConfig
+}
