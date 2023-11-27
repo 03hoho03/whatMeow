a0d94b3ac6d04d42ed2a1b990a9aa312a01c9e3a
03hoho03 <ghwjd321@naver.com>
2023-11-28 00:24:32 +0900

    style prettier 적용

--- frontend/app/api/account/login/social/kakao/callback/route.ts
+++ frontend/app/api/account/login/social/kakao/callback/route.ts
@@ -1,40 +1,42 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req:NextRequest) {
-  const code = req.nextUrl.searchParams.get('code');
-  
-  if(code) {
+export async function GET(req: NextRequest) {
+  const code = req.nextUrl.searchParams.get('code')
+
+  if (code) {
     const body = {
       code,
     }
-    const res = await fetch('http://127.0.0.1:7070/api/user/kakao',{
-    method:'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body:JSON.stringify(body)
+    const res = await fetch('http://127.0.0.1:7070/api/user/kakao', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
     })
-    if(res.status===200) {
+    if (res.status === 200) {
       const data = await res.json()
 
-      const accessToken=data?.accessToken
+      const accessToken = data?.accessToken
       const refreshToken = data?.refreshToken
 
-      const response = NextResponse.redirect('http://localhost:3000',{status:302})
-      response.cookies.set({
-        name:'accessToken',
-        value:accessToken,
-        httpOnly:true
+      const response = NextResponse.redirect('http://localhost:3000', {
+        status: 302,
       })
       response.cookies.set({
-        name:'refreshToken',
-        value:refreshToken,
-        httpOnly:true
+        name: 'accessToken',
+        value: accessToken,
+        httpOnly: true,
+      })
+      response.cookies.set({
+        name: 'refreshToken',
+        value: refreshToken,
+        httpOnly: true,
       })
       return response
-    } else{
-      return NextResponse.json({status:403})
+    } else {
+      return NextResponse.json({ status: 403 })
     }
   }
-  return NextResponse.json({status:403})
+  return NextResponse.json({ status: 403 })
 }
