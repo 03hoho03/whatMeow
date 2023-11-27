a0a18b17c3f8a7f581f3d1aeb4074a9d4753940f
03hoho03 <ghwjd321@naver.com>
2023-11-28 00:13:01 +0900

    feat 뒤로가기 버튼 추가

--- frontend/app/(route)/(auth)/join/page.tsx
+++ frontend/app/(route)/(auth)/join/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import JoinForm from './_components/JoinForm'
-import style from './page.module.css'
+import style from './JoinPage.module.css'
+import PageBackLink from './_components/PageBackLink'
 
 function JoinPage() {
   return (
-    <div className={style.main_wrapper}>
+    <div className={style.pageContainer}>
+      <PageBackLink />
       <JoinForm />
     </div>
   )
