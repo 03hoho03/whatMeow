313fd7667f40e81cb72c370213c44590203250be
03hoho03 <ghwjd321@naver.com>
2023-11-28 00:24:05 +0900

    style prettier 적용

--- frontend/app/api/account/login/social/kakao/route.ts
+++ frontend/app/api/account/login/social/kakao/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
-export async function GET () {
-  const RedirectUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}`;
-  return NextResponse.redirect(RedirectUrl);
+export async function GET() {
+  const RedirectUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}`
+  return NextResponse.redirect(RedirectUrl)
 }
