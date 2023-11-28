0ea5710069135599812ea597eb24bfa98f44be4b
03hoho03 <ghwjd321@naver.com>
2023-11-28 17:46:56 +0900

    feat 피드 내부 상호작용 메뉴

---
+++ frontend/app/(route)/(main)/[userId]/_components/FeedBottomMenu/index.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import style from './feedBottomMenu.module.css'
+import calcDate from '@/app/_utils/calcDate'
+import LikeBtn from '../LikeBtn'
+import CommentLink from '../CommentLink'
+import BookmarkBtn from '../BookmarkBtn'
+import LikeCount from '../LikeCount'
+
+interface FeedBottomMenuProps {
+  postId: string
+  createdAt: Date
+}
+
+const FeedBottomMenu = ({ createdAt, postId }: FeedBottomMenuProps) => {
+  const [time, setTime] = useState<string>(createdAt.toLocaleDateString())
+
+  useEffect(() => {
+    setTime(calcDate(createdAt))
+  }, [createdAt])
+
+  return (
+    <section className={style.main_wrapper}>
+      <div className={style.menu1}>
+        <div className={style.menu_btns}>
+          <LikeBtn postId={postId} />
+          <CommentLink postId={postId} />
+        </div>
+        <div className={style.menu_right}>
+          <span className={style.created_span}>{time}</span>
+          <BookmarkBtn />
+        </div>
+      </div>
+      <div>
+        <LikeCount postId={postId} />
+      </div>
+    </section>
+  )
+}
+
+export default FeedBottomMenu
