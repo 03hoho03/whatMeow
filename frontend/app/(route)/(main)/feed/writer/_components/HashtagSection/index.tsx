3c70dc6b47cc264966ddd63cc5140292d9be9496
03hoho03 <ghwjd321@naver.com>
2023-11-28 17:33:43 +0900

    feat 해쉬태그 섹션 분리

--- frontend/app/(route)/(main)/feed/writer/_components/HashtagSection/index.tsx
+++ frontend/app/(route)/(main)/feed/writer/_components/HashtagSection/index.tsx
@@ -3,7 +3,7 @@ import HashtagList from '../HashTagList'
 import style from './hashtagSection.module.css'
 import HashtagForm from '../HashtagForm'
 
-function HashTagForm() {
+const HashtagSection = () => {
   return (
     <section className={style.main_wrapper}>
       <h3 className={style.form_title}>해시태그</h3>
@@ -13,4 +13,4 @@ function HashTagForm() {
   )
 }
 
-export default HashTagForm
+export default HashtagSection
