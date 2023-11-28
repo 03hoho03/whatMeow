41c3914111da01dfffb6b8635c48a36261e97eb6
03hoho03 <ghwjd321@naver.com>
2023-11-28 17:31:06 +0900

    feat 함께있는고양이 기능 추가

--- frontend/app/(route)/(main)/feed/writer/_components/WriterForm/index.tsx
+++ frontend/app/(route)/(main)/feed/writer/_components/WriterForm/index.tsx
@@ -3,28 +3,45 @@ import React from 'react'
 import ImageUpload from '../ImageUpload'
 import ContextForm from '../ContextForm'
 import { SubmitHandler, useForm } from 'react-hook-form'
-import HashTagForm from '../HashtagSection'
+import HashtagSection from '../HashtagSection'
+import CatTagSection from '../CatTagSection'
 import { writer } from '@/app/_utils/constants'
 import SubmitBtn from '@/app/_common/SubmitBtn'
 import { useRecoilValue } from 'recoil'
 import { imageFileList } from '@/app/_store/atom/writer/image'
 import { hashtagList } from '@/app/_store/atom/writer/hashtag'
-import { useFetch } from '@/app/_helpers/client/useFetch'
+import { useFeedService } from '@/app/_services/feedService'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { catTagList } from '@/app/_store/atom/writer/catTag'
 
 interface WriterFormValue {
   content: string
 }
 
 const WriterForm = () => {
+  const route = useRouter()
   const fileList = useRecoilValue(imageFileList)
   const hashList = useRecoilValue(hashtagList)
-  const fetch = useFetch()
+  const catTag = useRecoilValue(catTagList)
+  const feedService = useFeedService()
   const {
     register,
     handleSubmit,
     watch,
     formState: { isValid },
   } = useForm<WriterFormValue>({ mode: 'onChange' })
+  const { mutate } = useMutation({
+    mutationFn: (form: FormData) => feedService.upload(form),
+    onSuccess: () => {
+      route.push('/')
+    },
+    onError: (error) => {
+      // status 따른 분기 처리 예정
+      console.log(error)
+    },
+  })
+
   const fields = {
     content: register('content', {
       required: '',
@@ -47,16 +64,19 @@ const WriterForm = () => {
       formData.append('files', file)
     })
     formData.append('content', content)
-    formData.append('tags', JSON.stringify(hashList))
+    catTag.forEach((tag) => formData.append('cat_ids', tag.id.toString()))
+    hashList.forEach((tag) => formData.append('tags', tag))
 
-    await fetch.post('/api/writer/submit', formData)
+    mutate(formData)
+    // await feedService.upload(formData)
   }
 
   return (
     <form onSubmit={handleSubmit(SubmitSuccess)}>
       <ImageUpload />
       <ContextForm content={fields.content} watch={watch} />
-      <HashTagForm />
+      <CatTagSection />
+      <HashtagSection />
       <SubmitBtn isValid={isValid}>
         <span>등록</span>
       </SubmitBtn>
