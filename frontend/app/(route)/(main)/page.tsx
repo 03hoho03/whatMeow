d1ec6569c1489e53bf30665e61ded2fa542da36b
03hoho03 <ghwjd321@naver.com>
2023-11-28 17:29:57 +0900

    chore 메인페이지 모듈수정

--- frontend/app/(route)/(main)/page.tsx
+++ frontend/app/(route)/(main)/page.tsx
@@ -1,4 +1,3 @@
-// import cn from 'classnames'
 import BookMarkList from '../../_components/BookMarkList'
 import MonthlyCats from '../../_components/MonthlyCats'
 import style from './page.module.css'
