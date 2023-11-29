bb1168ad11140e9f758ecfdca2411071c3d26a65
03hoho03 <ghwjd321@naver.com>
2023-11-30 03:28:46 +0900

    feat not modal 피드상세 페이지

---
+++ frontend/app/(route)/(main)/post/[postId]/components/CommentForm/index.tsx
@@ -0,0 +1,65 @@
+'use client'
+import React from 'react'
+import style from './commentForm.module.css'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import useCommentService, {
+  CommentUploadApiResponse,
+} from '@/app/_services/commentService'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+interface CommentFormReturn {
+  comment: string
+}
+interface CommentMutationInput {
+  postId: number
+  comment: string
+}
+
+const CommentForm = ({ postId }: { postId: number }) => {
+  const { register, handleSubmit, reset } = useForm<CommentFormReturn>()
+  const queryClient = useQueryClient()
+  const commentService = useCommentService()
+  const commentMutation = useMutation<
+    CommentUploadApiResponse,
+    Error,
+    CommentMutationInput
+  >({
+    mutationFn: ({ comment, postId }) => commentService.upload(comment, postId),
+  })
+
+  const handleUploadComment: SubmitHandler<CommentFormReturn> = ({
+    comment,
+  }) => {
+    commentMutation.mutate(
+      { comment, postId },
+      {
+        onSuccess: () => {
+          reset()
+          queryClient.invalidateQueries({
+            queryKey: ['hydrate-postDetail', postId],
+          })
+        },
+        onError: (error) => {
+          console.log(error)
+        },
+      },
+    )
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleUploadComment)}
+      className={style.commentForm}
+    >
+      <div className={style.commentInputContainer}>
+        <input
+          {...register('comment', { required: true })}
+          placeholder="댓글 달기"
+        />
+        <button type="submit">게시</button>
+      </div>
+    </form>
+  )
+}
+
+export default CommentForm
---
+++ frontend/app/(route)/(main)/post/[postId]/components/Post/index.tsx
@@ -0,0 +1,70 @@
+'use client'
+import React, { useEffect } from 'react'
+import style from './post.module.css'
+import PostMedia from '../PostMedia'
+import LikeBtn from '../LikeBtn'
+import BookmarkBtn from '../BookmarkBtn'
+import LikeCount from '../LikeCount'
+import CommentForm from '../CommentForm'
+import WriterInfoHeader from '../WriterInfoHeader'
+import PostContent from '../PostContent'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useFeedService } from '@/app/_services/feedService'
+
+interface PostProps {
+  postId: number
+}
+
+const Post = ({ postId }: PostProps) => {
+  const feedService = useFeedService()
+  const queryClient = useQueryClient()
+  const { data, isFetching, isSuccess, isFetched } = useQuery({
+    queryKey: ['hydrate-postDetail', postId],
+    queryFn: () => feedService.getPostDetail(postId),
+  })
+
+  useEffect(() => {
+    queryClient.setQueryData(['like', postId], data?.like)
+  }, [isFetched])
+
+  if (isFetching) return <p>...로딩중</p>
+  if (isSuccess) {
+    return (
+      <div className={style.postWrapper}>
+        <WriterInfoHeader
+          writerThumnail={data?.writerThumnail}
+          nickname={data?.nickname}
+        />
+        <PostMedia images={data?.images} />
+        <div className={style.noneMediaContainer}>
+          <header className={style.userInfoContainer}>
+            <div className={style.writerThumnailWrapper}>
+              <img src={data.writerThumnail} />
+            </div>
+            <div>{data.nickname}</div>
+          </header>
+          <PostContent
+            content={data?.content}
+            hashtags={data?.hashtags}
+            comments={data.comments}
+          />
+          <div className={style.userInteractContainer}>
+            <div className={style.userInteractBtnsContainer}>
+              <LikeBtn postId={data?.postId} />
+              <BookmarkBtn />
+            </div>
+            <div className={style.likeCountContainer}>
+              <LikeCount postId={data?.postId} />
+            </div>
+            <div className={style.createdAtContainer}>
+              <span>{`${data?.createdAt}`}</span>
+            </div>
+            <CommentForm postId={data?.postId} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Post
