90b8f2837be29cc6b1cc62ef9d603cb38908a5f9
03hoho03 <ghwjd321@naver.com>
2023-11-29 22:59:33 +0900

    refact modal 위치변경

--- frontend/app/(route)/(main)/layout.tsx
+++ frontend/app/(route)/(main)/layout.tsx
@@ -2,10 +2,17 @@ import Navbar from '@/app/_components/Navbar'
 import BottomMenu from '@/app/_common/BottomMenu'
 import React from 'react'
 
-const layout = ({ children }: { children: React.ReactNode }) => (
+const layout = ({
+  children,
+  modal,
+}: {
+  children: React.ReactNode
+  modal: React.ReactNode
+}) => (
   <div>
     <Navbar />
     {children}
+    {modal}
     <BottomMenu />
   </div>
 )
