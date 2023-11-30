25327ec342277fbe7e570322ea3aa43a79d7b8b9
03hoho03 <ghwjd321@naver.com>
2023-11-30 16:25:14 +0900

    고양이 프로필

--- frontend/app/(route)/(main)/catprofile/[catId]/components/Profile/index.tsx
+++ frontend/app/(route)/(main)/catprofile/[catId]/components/Profile/index.tsx
@@ -2,16 +2,24 @@
 import React from 'react'
 import style from './profile.module.css'
 
-const Profile = () => {
+const Profile = ({
+  catName,
+  thumnail,
+  explain,
+}: {
+  catName: string
+  thumnail: string
+  explain: string
+}) => {
   return (
     <section className={style.profileContainer}>
       <div className={style.profileImgWrapper}>
-        <div className={style.ProfileImg}></div>
+        <img src={thumnail} alt="프로필썸네일" className={style.ProfileImg} />
       </div>
       <div className={style.catNameWrapper}>
-        <span className={style.catName}>네로</span>
+        <span className={style.catName}>{catName}</span>
       </div>
-      <p className={style.catIntroduce}>{'꽤 귀여워요'}</p>
+      <p className={style.catIntroduce}>{explain}</p>
       <button type="button" className={style.EditBtn}>
         프로필 편집
       </button>
---
+++ frontend/app/(route)/(main)/catprofile/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const catProfilePage = () => {
+  return <div>catProfilePage</div>
+}
+
+export default catProfilePage
