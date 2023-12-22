594f0cec435cede1d5b7a548254d15526f3fc65c
fnzksxl <fnzksxl@gmail.com>
2023-12-22 12:15:39 +0900

    #59 Fix: Prefix url 수정

--- backend/app/api/v2/__init__.py
+++ backend/app/api/v2/__init__.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from .user import controller as user
 
 router = APIRouter()
-router.include_router(user.router, prefix="/users")
+router.include_router(user.router, prefix="/v2/users")
