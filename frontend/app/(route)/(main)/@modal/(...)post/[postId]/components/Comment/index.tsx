ed9e04494d13c04d14442d36d31016cbd11a5705
03hoho03 <ghwjd321@naver.com>
2023-12-28 17:11:32 +0900

    Feat: hover 시 댓글관리 옵션 버튼표시

--- frontend/app/(route)/(main)/@modal/(...)post/[postId]/components/Comment/index.tsx
+++ frontend/app/(route)/(main)/@modal/(...)post/[postId]/components/Comment/index.tsx
@@ -1,28 +1,52 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import style from './comment.module.css'
+import CommentOptionDialog from '../CommentOptionDialog'
 
 interface CommentProps {
   comment: CommentInfo
+  postId: number
 }
 interface CommentInfo {
+  commentId: number
   comment: string
+  createdAt: string
   nickname: string
   thumnail: string
 }
 
-const Comment = ({ comment }: CommentProps) => {
+const Comment = ({ comment, postId }: CommentProps) => {
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  const handleMouseOver = () => {
+    setIsHover(true)
+  }
+  const handleMouseLeave = () => {
+    setIsHover(false)
+  }
+
   return (
-    <li className={style.commentContainer}>
-      <div className={style.writerThumnailContainer}>
-        <div className={style.writerThumnailWrapper}>
-          <img src={comment.thumnail} />
+    <li
+      className={style.commentContainer}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={style.commentWrapper}>
+        <div className={style.writerThumnailContainer}>
+          <div className={style.writerThumnailWrapper}>
+            <img src={comment.thumnail} />
+          </div>
+        </div>
+        <div className={style.commentInfoContainer}>
+          <span>{comment.nickname}</span>
+          <p>{comment.comment}</p>
         </div>
       </div>
-      <div className={style.commentInfoContainer}>
-        <span>{comment.nickname}</span>
-        <p>{comment.comment}</p>
-      </div>
+      <CommentOptionDialog
+        commentId={comment.commentId}
+        postId={postId}
+        isHover={isHover}
+      />
     </li>
   )
 }
