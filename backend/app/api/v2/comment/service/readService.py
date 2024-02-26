178211cc445cadd6ce7ad68e56741085c50c2f71
Taewon <71972587+fnzksxl@users.noreply.github.com>
2024-02-26 21:30:00 +0800

    Fix: Fix Corutine Error

--- backend/app/api/v2/comment/service/readService.py
+++ backend/app/api/v2/comment/service/readService.py
@@ -1,6 +1,7 @@
 from ..utils import databases
 
+
 async def lookupComment(postId, db):
-    comments = databases.find_commets_by_post_id(postId, db)
-  
+    comments = await databases.find_commets_by_post_id(postId, db)
+
     return comments
