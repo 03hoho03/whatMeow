a3c4b708c72a7c3bcc7d2d3a7feb7ad73f89fc9e
03hoho03 <ghwjd321@naver.com>
2024-03-13 17:28:21 +0900

    Refactor: 최근피드 InfiniteQuery 커스텀 훅화

--- frontend/app/(route)/(main)/explore/_components/FeedList/index.tsx
+++ frontend/app/(route)/(main)/explore/_components/FeedList/index.tsx
@@ -1,27 +1,13 @@
 'use client'
 import React, { useRef } from 'react'
 import Feed from '../Feed'
-import { useInfiniteQuery } from '@tanstack/react-query'
 import { useObserver } from '@/app/_hooks/useUserObserver'
-import { FEED_SIZE } from '@/app/_utils/constants'
-import useGuestService from '@/app/_services/guestService'
-
-interface FeedItem {
-  createdAt: Date
-  content: string
-  images: string[]
-  like: Like
-  nickname: string
-  writerThumnail: string
-  postId: number
-}
-interface Like {
-  count: number
-  isLike: boolean
-}
+import {
+  SelectedPost,
+  useRecentPostListQuery,
+} from '@/app/_services/quries/useRecentPostList'
 
 const FeedList = () => {
-  const guestService = useGuestService()
   const bottom = useRef<HTMLDivElement | null>(null)
   const {
     data,
@@ -30,18 +16,8 @@ const FeedList = () => {
     fetchNextPage,
     isFetchingNextPage,
     hasNextPage,
-  } = useInfiniteQuery({
-    queryKey: ['hydrate-feeds'],
-    queryFn: ({ pageParam }) =>
-      guestService.getRecentList(pageParam, FEED_SIZE),
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, allPages) => {
-      if (lastPage.length < FEED_SIZE) {
-        return null
-      }
-      return allPages.length // 다음 페이지 번호
-    },
-  })
+  } = useRecentPostListQuery()
+
   const onIntersect = (entries: IntersectionObserverEntry[]) => {
     if (status === 'error') {
       console.log(error)
@@ -61,10 +37,10 @@ const FeedList = () => {
   return (
     <div>
       {status === 'pending' && <p>불러오는 중</p>}
-      {status === 'error' && <p>{error.message}</p>}
+      {status === 'error' && <p>{error?.message}</p>}
       {status === 'success' &&
-        data.pages.map((group) =>
-          group.map((feed: FeedItem, idx: number) => (
+        data?.pages.map((group) =>
+          group.map((feed: SelectedPost, idx: number) => (
             <Feed key={idx} feed={feed} />
           )),
         )}
