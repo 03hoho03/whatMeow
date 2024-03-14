4fb575101907a0f194538455970fbca1769346f7
03hoho03 <ghwjd321@naver.com>
2024-03-14 13:34:58 +0900

    Refactor: v2 api 주소 적용 및 api body 수정

--- frontend/app/_services/commentService.ts
+++ frontend/app/_services/commentService.ts
@@ -2,35 +2,44 @@ import { useFetch } from '../_helpers/client/useFetch'
 import { BASE_URL } from '../_utils/constants'
 
 export interface CommentUploadApiResponse {
-  success: boolean
+  comment: string
+  id: number
+  uploader: number
+  postId: number
+  nickname: string
+  createdAt: string
 }
 interface Comment {
   commentId: number
   comment: string
-  createdAt: string
   nickname: string
   thumnail: string
+}
+export interface CommentListApiResponse extends Comment {
+  uploader: number
+  createdAt: string
 }
 
 interface CommentService {
   upload: (comment: string, postId: number) => Promise<CommentUploadApiResponse>
   delete: (commentId: number) => Promise<{ success: boolean }>
-  getList: (postId: number) => Promise<Comment[]>
+  getList: (postId: number) => Promise<CommentListApiResponse[]>
 }
 
 function useCommentService(): CommentService {
   const fetch = useFetch()
-  const baseUrl = `${BASE_URL}/api/v1/comment`
+  const baseUrl = `${BASE_URL}/api/v2/comment`
   return {
     upload: async (comment, postId) => {
-      const body = {
+      const uploadBody = {
         comment,
       }
+
       const response = await fetch.post(`${baseUrl}/${postId}`, {
         headers: {
           'Content-type': 'application/json',
         },
-        body,
+        body: uploadBody,
         options: {
           credentials: 'include',
         },
