4e33f14f8e4f27bd6247d7fabf651476eb65d950
03hoho03 <ghwjd321@naver.com>
2024-03-14 13:34:41 +0900

    Refactor: v2 api 주소 적용

--- frontend/app/_services/catService.ts
+++ frontend/app/_services/catService.ts
@@ -22,10 +22,10 @@ interface Post {
 
 function useCatService(): CatService {
   const fetch = useFetch()
-  const baseUrl = `${BASE_URL}/api/v1/cat`
+  const baseUrl = `${BASE_URL}/api/v2/cat`
   return {
     profileUpload: async (form) => {
-      const response = await fetch.post(`${baseUrl}/add`, {
+      const response = await fetch.post(`${baseUrl}`, {
         body: form,
         options: { credentials: 'include' },
       })
